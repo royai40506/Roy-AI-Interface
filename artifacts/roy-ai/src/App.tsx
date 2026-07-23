cd2f969c3f875ed5fec60ffff099cd04b4fd975c
royai40506 <rahulpatil123808@gmail.com>
2026-07-23 10:13:05 +0530

    Temporary HomePage test

--- artifacts/roy-ai/src/App.tsx
+++ artifacts/roy-ai/src/App.tsx
@@ -1,13 +1,5 @@
-import Sidebar from './components/Sidebar';
-import HomePage from './pages/HomePage';
+import HomePage from '@/pages/HomePage';
 
 export default function App() {
-  return (
-    <div className="h-screen w-full flex">
-      <Sidebar />
-      <div style={{ flex: 1 }}>
-        <HomePage />
-      </div>
-    </div>
-  );
+  return <HomePage />;
 }
